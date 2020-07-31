4c4a331aa5447416265f70fdb4885489fdb68c92
Juan Ceschin <juanceschin@gmail.com>
2020-07-30 21:02:10 -0300

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,78 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_012800) do
+ActiveRecord::Schema.define(version: 2020_07_31_000022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "daily_targets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "caloric_target"
+    t.integer "protein_target"
+    t.integer "carb_target"
+    t.integer "fat_target"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_daily_targets_on_user_id"
+  end
+
+  create_table "item_categories", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_item_categories_on_category_id"
+    t.index ["item_id"], name: "index_item_categories_on_item_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "seller_id", null: false
+    t.string "name"
+    t.text "description"
+    t.integer "calories"
+    t.integer "proteins"
+    t.integer "fats"
+    t.integer "carbs"
+    t.string "origin"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["seller_id"], name: "index_items_on_seller_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "order_id", null: false
+    t.time "consumed_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_order_items_on_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "sellers", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +95,11 @@ ActiveRecord::Schema.define(version: 2020_07_29_012800) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "daily_targets", "users"
+  add_foreign_key "item_categories", "categories"
+  add_foreign_key "item_categories", "items"
+  add_foreign_key "items", "sellers"
+  add_foreign_key "order_items", "items"
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "orders", "users"
 end
