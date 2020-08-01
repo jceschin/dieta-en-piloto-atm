c192ae88111345bdcc6f02ac6ba7a2fb9acd53fa
Juan Ceschin <juanceschin@gmail.com>
2020-08-01 15:41:55 -0300

    Add validation for positive values

--- app/models/daily_target.rb
+++ app/models/daily_target.rb
@@ -4,4 +4,9 @@ class DailyTarget < ApplicationRecord
   validates :protein_target, presence: true
   validates :carb_target, presence: true
   validates :fat_target, presence: true
+
+  validates_numericality_of :caloric_target, greater_than: 0
+  validates_numericality_of :protein_target, greater_than: 0
+  validates_numericality_of :carb_target, greater_than: 0
+  validates_numericality_of :fat_target, greater_than: 0
 end
