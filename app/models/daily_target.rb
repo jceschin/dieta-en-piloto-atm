9bcda60c3b30ad60f9f8f3669ed18fffd0003186
Juan Ceschin <juanceschin@gmail.com>
2020-08-01 11:47:18 -0300

    Add all validations

--- app/models/daily_target.rb
+++ app/models/daily_target.rb
@@ -1,3 +1,7 @@
 class DailyTarget < ApplicationRecord
   belongs_to :user
+  validates :caloric_target, presence: true
+  validates :protein_target, presence: true
+  validates :carb_target, presence: true
+  validates :fat_target, presence: true
 end
