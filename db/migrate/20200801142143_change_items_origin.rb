dec9dcd60f0834f43173c43d4d3c793db64aeeea
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-08-01 11:47:55 -0300

    Addesd emigrations to change string to integer for enums:

--- db/migrate/20200801142143_change_items_origin.rb
+++ db/migrate/20200801142143_change_items_origin.rb
@@ -1,5 +1,5 @@
 class ChangeItemsOrigin < ActiveRecord::Migration[6.0]
   def change
-    change_column :items, :origin, :integer
+    change_column :items, :origin, 'integer USING CAST(origin AS integer)'
   end
 end
--- db/migrate/20200801142157_change_orders_status.rb
+++ db/migrate/20200801142157_change_orders_status.rb
@@ -1,5 +1,5 @@
 class ChangeOrdersStatus < ActiveRecord::Migration[6.0]
   def change
-    change_column :orders, :status, :integer
+    change_column :orders, :status, 'integer USING CAST(status AS integer)'
   end
 end
