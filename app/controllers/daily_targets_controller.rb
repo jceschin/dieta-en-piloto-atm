6538db857e078de157f45536f6a2cffa54a5b1d1
Juan Ceschin <juanceschin@gmail.com>
2020-08-01 12:06:43 -0300

    Add #new and #create actions in controller

--- app/controllers/daily_targets_controller.rb
+++ app/controllers/daily_targets_controller.rb
@@ -1,2 +1,16 @@
 class DailyTargetsController < ApplicationController
+  def new
+    @daily_target = DailyTarget.new
+  end
+
+  def create
+    @daily_target = DailyTarget.new(daily_target_params)
+    @daily_target.user = current_user
+  end
+
+  private
+
+  def daily_target_params
+    params.require(:daily_target).permit(:caloric_target, :protein_target, :carb_target, :fat_target)
+  end
 end
