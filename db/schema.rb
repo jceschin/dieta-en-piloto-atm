455ba91d4f6bab7dbb8fa38128ae5822982f2821
Armando Teran <ajteranv@gmail.com>
2020-08-05 17:47:39 -0300

    removed picture column from items table created but unnecessary because we will use  Cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_170838) do
+ActiveRecord::Schema.define(version: 2020_08_05_204458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 2020_08_05_170838) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "picture"
     t.index ["seller_id"], name: "index_items_on_seller_id"
   end
 
