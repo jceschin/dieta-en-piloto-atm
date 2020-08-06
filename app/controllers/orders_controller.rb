9398685e5191d2c375c1e5e9b79ace822be18d78
Juan Ceschin <juanceschin@gmail.com>
2020-08-06 19:51:54 -0300

    Create item with strong params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,11 @@
 class OrdersController < ApplicationController
+  def create
+    @order_item = OrderItem.new(order_item_params)
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:item_id, :order_id, :consumed_at)
+  end
 end
