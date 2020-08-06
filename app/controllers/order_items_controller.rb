2dd3d248e2845f656ba6b37aa961f9bb044c1715
Juan Ceschin <juanceschin@gmail.com>
2020-08-06 19:35:42 -0300

    Deleted code in order_items controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,14 +1,2 @@
 class OrderItemsController < ApplicationController
-  def create
-    @order_item = OrderItem.new
-    @order_item.user = current_user
-    # authorize @order_item
-    @order_item.item = Item.find(params[:item_id])
-
-    if @order_item.save
-      redirect_to item_path(Item.find(params[:item_id]))
-    else
-      render "items/show"
-    end
-  end
 end
