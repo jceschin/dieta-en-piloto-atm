10dc2f92f6c3db0092526545d70afeb69127338d
Armando Teran <ajteranv@gmail.com>
2020-08-08 14:21:50 -0300

    working on query de sugerencias

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   def index
+    dailytargets = DailyTarget.where(user_id:current_user.id)
     @items = policy_scope(Item)
+    @items = Item.where("calories<=""#{dailytargets.last.caloric_target}")
     @categories = Category.all
   end
 
