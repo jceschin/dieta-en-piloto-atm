4c1607ea05480083ec1de84935bcdbde8edae12c
Juan Ceschin <juanceschin@gmail.com>
2020-08-08 12:06:18 -0300

    Add create method for order_items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,6 +2,11 @@ class OrderItemsController < ApplicationController
   def create
     params[:quantity].times do
       @order_item = OrderItem.new(order_item_params)
+      if (order = current_user.pending_order)
+      else
+        order = Order.create(user_id: current_user.id, status: :pending)
+      end
+      @order_item.order == order
     end
   end
 
