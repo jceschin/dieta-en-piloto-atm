76790058b2b879f64371109011913d4bf6e2f70c
Juan Ceschin <juanceschin@gmail.com>
2020-08-11 20:06:27 -0300

    Took if-else statement out of params[:quantity].times

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,12 +1,12 @@
 class OrderItemsController < ApplicationController
   def create
+    if !current_user.pending_order.nil?
+      order = current_user.pending_order
+    else
+      order = Order.create(user_id: current_user.id, status: :pending)
+    end
     params[:quantity].times do
       @order_item = OrderItem.new(order_item_params)
-      if !current_user.pending_order.nil?
-        order = current_user.pending_order
-      else
-        order = Order.create(user_id: current_user.id, status: :pending)
-      end
       @order_item.order = order
       @order_item.save
     end
