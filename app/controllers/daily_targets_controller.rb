74c7cab90b3b54757b9f6b4f20ef8b18bc5906e6
Armando Teran <ajteranv@gmail.com>
2020-08-12 11:23:40 -0300

    added calculation of macros left -can be refactored-

--- app/controllers/daily_targets_controller.rb
+++ app/controllers/daily_targets_controller.rb
@@ -20,6 +20,10 @@ class DailyTargetsController < ApplicationController
 
   def edit
     authorize @daily_target
+    cal_left
+    proteins_left
+    carbs_left
+    fats_left
   end
 
   def update
@@ -36,5 +40,49 @@ class DailyTargetsController < ApplicationController
 
   def daily_target_params
     params.require(:daily_target).permit(:caloric_target, :protein_target, :carb_target, :fat_target, :control_limit)
+  end
+
+  def cal_left
+    orders = Order.where(user_id:current_user.id)
+    @daily_target = DailyTarget.find(params[:id])
+    @cal_left = @daily_target.caloric_target
+    orders.each do |order|
+      order.order_items.each do |item|
+        @cal_left -= Item.find_by_id(item.item_id).calories
+      end
+    end
+  end
+
+  def proteins_left
+    orders = Order.where(user_id:current_user.id)
+    @daily_target = DailyTarget.find(params[:id])
+    @proteins_left = @daily_target.protein_target
+    orders.each do |order|
+      order.order_items.each do |item|
+        @proteins_left -= Item.find_by_id(item.item_id).proteins
+      end
+    end
+  end
+
+  def carbs_left
+    orders = Order.where(user_id:current_user.id)
+    @daily_target = DailyTarget.find(params[:id])
+    @carbs_left = @daily_target.carb_target
+    orders.each do |order|
+      order.order_items.each do |item|
+        @carbs_left -= Item.find_by_id(item.item_id).carbs
+      end
+    end
+  end
+
+  def fats_left
+    orders = Order.where(user_id:current_user.id)
+    @daily_target = DailyTarget.find(params[:id])
+    @fats_left = @daily_target.fat_target
+    orders.each do |order|
+      order.order_items.each do |item|
+        @fats_left -= Item.find_by_id(item.item_id).fats
+      end
+    end
   end
 end
