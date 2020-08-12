d162ddfc74976648679316d5a633872896631c96
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-08-12 01:03:30 -0300

    several improvements

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,7 @@
 class ItemsController < ApplicationController
   def index
     @items = policy_scope(Item)
+    @order_item = OrderItem.new
     @categories = Category.all
-  end
-
-  def show
   end
 end
