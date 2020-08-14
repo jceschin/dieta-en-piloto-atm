65d410e5212005396eb94016ff58424c89b5d67e
Armando Teran <ajteranv@gmail.com>
2020-08-14 20:51:18 -0300

    added consumo edit route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :daily_targets, only: %i[new create show edit update]
 
   resources :items, only: %i[index new create] do
-    resources :order_items, only: %i[create]
+    resources :order_items, only: %i[create edit]
   end
 end
