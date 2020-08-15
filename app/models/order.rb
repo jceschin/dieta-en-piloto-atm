e113a6871a6a82338a2142521132ad4feb7f926f
Armando Teran <ajteranv@gmail.com>
2020-08-15 12:54:14 -0300

    working on cal count to show message

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,19 @@ class Order < ApplicationRecord
     end
     total
   end
+
+  def total_cal
+    total_cal = 0
+    items.each do |item|
+      total_cal += item.calories
+    end
+    total_cal
+  end
+
+  def total_cal_in_target?
+    daily_target = DailyTarget.find(user_id:user.id)
+    if daily_target_nil?
+      total_cal == daily_target.caloric_target
+    end
+  end
 end
