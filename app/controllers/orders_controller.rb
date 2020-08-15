16409ea0cafc89ef847ed36337ec9fba10cd0644
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-08-15 15:03:01 -0300

    updating order, front unfinished

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,20 @@
 class OrdersController < ApplicationController
   def update
+    # @order = Order.new(order_params)
+    @order = current_user.pending_order
+    @order.status = :finished
+    if @order.save
+      redirect_to items_path
+    # else
+    # render 'items'
+    end
 
+    authorize @order
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:id)
   end
 end
---
+++ app/policies/order_policy.rb
@@ -0,0 +1,13 @@
+class OrderPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def update?
+    record.user == user
+    # - record: the order passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+end
