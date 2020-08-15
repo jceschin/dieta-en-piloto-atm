90ee51532d383389492cd51235e3df221a68dd36
Armando Teran <ajteranv@gmail.com>
2020-08-15 16:42:33 -0300

    changed has_many:daily_targets to has_one :daily_target

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :orders
-  has_many :daily_targets
+  has_one :daily_target
   has_one_attached :photo
 
   def pending_order
