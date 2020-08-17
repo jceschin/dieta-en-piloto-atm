b07c1c66414fe372f113df81854f8f2a0e720bf0
Juan Ceschin <juanceschin@gmail.com>
2020-08-17 12:28:27 -0300

    Move nutrients_left method from items controller to user model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,66 +32,18 @@ class ItemsController < ApplicationController
   end
 
   def calories_in_target?(item)
-    item.calories <= cal_left * "1.#{@dailytarget.control_limit}".to_f && item.calories >= cal_left * (1 - "0.#{@dailytarget.control_limit}".to_f)
+    item.calories <= current_user.cal_left * "1.#{@dailytarget.control_limit}".to_f && item.calories >= current_user.cal_left * (1 - "0.#{@dailytarget.control_limit}".to_f)
   end
 
   def proteins_in_target?(item)
-    item.proteins <= proteins_left * "1.#{@dailytarget.control_limit}".to_f && item.proteins >= proteins_left * (1 - "0.#{@dailytarget.control_limit}".to_f)
+    item.proteins <= current_user.proteins_left * "1.#{@dailytarget.control_limit}".to_f && item.proteins >= current_user.proteins_left * (1 - "0.#{@dailytarget.control_limit}".to_f)
   end
 
   def carbs_in_target?(item)
-    item.carbs <= carbs_left * "1.#{@dailytarget.control_limit}".to_f && item.carbs >= carbs_left * (1 - "0.#{@dailytarget.control_limit}".to_f)
+    item.carbs <= current_user.carbs_left * "1.#{@dailytarget.control_limit}".to_f && item.carbs >= current_user.carbs_left * (1 - "0.#{@dailytarget.control_limit}".to_f)
   end
 
   def fats_in_target?(item)
-    item.fats <= fats_left * "1.#{@dailytarget.control_limit}".to_f && item.fats >= fats_left * (1 - "0.#{@dailytarget.control_limit}".to_f)
-  end
-
-  def cal_left
-    orders = Order.where(user_id: current_user.id)
-    @daily_target = DailyTarget.find_by(user_id: current_user.id)
-    @cal_left = @daily_target.caloric_target
-    orders.each do |order|
-      order.order_items.each do |item|
-        @cal_left -= Item.find_by_id(item.item_id).calories if item.consumed_at&.today?
-      end
-    end
-    @cal_left
-  end
-
-  def proteins_left
-    orders = Order.where(user_id: current_user.id)
-    @daily_target = DailyTarget.find_by(user_id: current_user.id)
-    @proteins_left = @daily_target.protein_target
-    orders.each do |order|
-      order.order_items.each do |item|
-        @proteins_left -= Item.find_by_id(item.item_id).proteins if item.consumed_at&.today?
-      end
-    end
-    @proteins_left
-  end
-
-  def carbs_left
-    orders = Order.where(user_id: current_user.id)
-    @daily_target = DailyTarget.find_by(user_id: current_user.id)
-    @carbs_left = @daily_target.carb_target
-    orders.each do |order|
-      order.order_items.each do |item|
-        @carbs_left -= Item.find_by_id(item.item_id).carbs if item.consumed_at&.today?
-      end
-    end
-    @carbs_left
-  end
-
-  def fats_left
-    orders = Order.where(user_id: current_user.id)
-    @daily_target = DailyTarget.find_by(user_id: current_user.id)
-    @fats_left = @daily_target.fat_target
-    orders.each do |order|
-      order.order_items.each do |item|
-        @fats_left -= Item.find_by_id(item.item_id).fats if item.consumed_at&.today?
-      end
-    end
-    @fats_left
+    item.fats <= current_user.fats_left * "1.#{@dailytarget.control_limit}".to_f && item.fats >= current_user.fats_left * (1 - "0.#{@dailytarget.control_limit}".to_f)
   end
 end
