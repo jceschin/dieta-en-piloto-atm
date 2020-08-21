0042c0e47a1b55d9223feda5be2a7bf62aaf63d7
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-08-20 22:10:55 -0300

    commit for merge with master

--- app/javascript/components/increment_buttons.js
+++ app/javascript/components/increment_buttons.js
@@ -6,22 +6,65 @@ const initIncrementButtons = () => {
   $(".btn-inc-dec").on("click", function() {
     event.preventDefault();
     var $button = $(this);
-    var oldValue = $button.parent().find("input").val();
+    const $input = $button.parent().find("input")
+    var oldValue = $input.val();
+
+    var newVal;
+    var incrementType;
 
     if ($button.text() == "+") {
-      var newVal = parseFloat(oldValue) + 1;
+      newVal = parseFloat(oldValue) + 1;
+      incrementType = "inc";
     } else {
      // Don't allow decrementing below zero
       if (oldValue > 1) {
-        var newVal = parseFloat(oldValue) - 1;
+        newVal = parseFloat(oldValue) - 1;
       } else {
         newVal = 1;
       }
+      incrementType = "dec";
     }
 
+    // updates the value of the input
     $button.parent().find("input").val(newVal);
 
+    const idName = $input.attr('id');
+
+    // Optional change to total value, if the clases are note in the html
+    // it does nothing
+    if (idName == 'item-quantity'){
+      // case for the item_Modal
+      const $priceP = $(`#${idName}-total`)
+      $priceP.text(`$${changeTotalItem($priceP, oldValue, newVal)}`);
+    } else {
+      // case for the cart modal
+      const $priceP = $(`#cart-total-price`)
+      $priceP.text(`$${changeTotalCart($priceP, incrementType)}`);
+    }
   });
 };
+
+
+const changeTotalItem = ($element, oldQty, newQty) => {
+  console.log(`${oldQty} ${newQty}`);
+  if ( $element.length ){
+    const oldValue = parseFloat($element.text().replace('$', ''));
+    const singleValue = oldValue / oldQty;
+    return(singleValue * newQty);
+  }
+};
+
+const changeTotalCart = ($element, $total, incrementType) => {
+  console.log(`${incrementType}`);
+  if ( $element.length ){
+    const oldValue = parseFloat($element.text().replace('$', ''));
+    const singleValue = oldValue / oldQty;
+    return(singleValue * newQty);
+    // console.log(`this: ${oldValue}`);
+  }
+  // const partial =
+  // accessory = (raining ? "umbrella" : "sunglasses")
+};
+
 
 export { initIncrementButtons };
