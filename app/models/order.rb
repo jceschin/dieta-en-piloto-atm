7f17ff235cbde766311dc21e0e83a24dedbf63df
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-08-21 00:13:54 -0300

    Added feature tested and working

--- app/models/order.rb
+++ app/models/order.rb
@@ -23,4 +23,22 @@ class Order < ApplicationRecord
   def total_cal_in_target?
     total_cal <= DailyTarget.find_by(user_id: user.id).caloric_target
   end
+
+  # returns a hash with it's items key as id
+  # and the quantity of each as value
+  def quantify
+    oi_hash = Hash.new(0)
+    order_items.each do |o_item|
+      oi_hash[o_item.item.id] += 1
+    end
+    oi_hash
+  end
+
+  def items_and_qty
+    iq_hash = Hash.new([Item.new, 0])
+    quantify.map do |key, value|
+      iq_hash[key] = [Item.find(key), value]
+    end
+    iq_hash
+  end
 end
