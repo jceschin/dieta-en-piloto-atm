b690f92dd16eacecb3aa19979624ca761ac04a89
Armando Teran <ajteranv@gmail.com>
2020-08-25 20:15:27 -0300

    merged updated seed master and fixed range in carousel

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,88 +17,9 @@ require_relative 'seeds/categories'
 # Deletes all table data in db
 reset_data
 
-ITEMS_INFO = [
-  {
-    name: "Ensalada Mikonos" ,
-    description: "Atún, rúcula, espinaca, aceitunas negras, croutones, palta, tomates y queso sardo...",
-    calories: 451,
-    proteins: 30,
-    fats: 30,
-    carbs: 14,
-    origin: :seller,
-    price: 1000,
-    categories_keywords: ["ensaladas", "pescados"],
-    picture: "https://res.cloudinary.com/ajtvlggc/image/upload/v1596648435/rh9nfmwoba1hgjxmrx3o.jpg"
-  },
-    {
-    name: "Ensalada Crispy" ,
-    description: "Lechuga romana, tomates cherry, pepino, mango, queso mozarella, pechuga de pollo, ajo, sal y pimienta.",
-    calories: 285,
-    proteins: 24,
-    fats: 15,
-    carbs: 14,
-    origin: :seller,
-    price: 700,
-    categories_keywords: ["ensaladas", "pollos"],
-    picture: "https://res.cloudinary.com/ajtvlggc/image/upload/v1596648827/r5r5bezu4uaxdalw8nt4.png"
-  },
-  {
-    name: "Burger Zarpada" ,
-    description: "Doble carne con doble cheddar, mostaza, ketchup y cebolla brunoise.",
-    calories: 490,
-    proteins: 22,
-    fats: 32,
-    carbs: 31,
-    origin: :seller,
-    price: 500,
-    categories_keywords: ["hamburguesas", "carnes"],
-    picture: "https://res.cloudinary.com/ajtvlggc/image/upload/v1596649435/dbqozlifg2b2fxwwzrul.png"
-  },
-      {
-    name: "Pasta Bolognese" ,
-    description: "pasta aechuga de pollo, aceite de oliva, panceta, cebolla. salsa inglesa",
-    calories: 488,
-    proteins: 26,
-    fats: 25,
-    carbs: 37,
-    origin: :seller,
-    price: 450,
-    categories_keywords: ["pastas", "carnes"],
-    picture: "https://res.cloudinary.com/ajtvlggc/image/upload/v1596654426/i30zaepgl1soxwwqk2kc.jpg"
-  }
-]
-
-CATEGORIES_INFO = [
-  {
-    name: "ensaladas",
-  },
-  {
-    name: "hamburguesas",
-  },
-  {
-    name: "pollos",
-  },
-  {
-    name: "carnes",
-  },
-  {
-    name: "postres",
-  },
-  {
-    name: "pizzas",
-  },
-  {
-    name: "pastas",
-  },
-  {
-    name: "pescados",
-  }
-]
-
 puts 'Creating Categories'
 # Important! when adding a category, add also the picture with its same name
 # in app/assets/images/categories/%category_name%.jpg
-
 CATEGORIES_INFO.each do |info|
   puts "--creating #{info[:name]}"
   Category.create!(
