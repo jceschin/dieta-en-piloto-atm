693c97cd530742a40cb1cdedc14c62c89c213c6d
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-08-27 18:24:56 -0300

    Categories reworked back and front wip

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,22 +2,29 @@ class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   # pundit
-  skip_after_action :verify_authorized, only: :set_items
-  after_action :verify_policy_scoped, only: :set_items, unless: :skip_pundit?
+  skip_after_action :verify_authorized, only: :select_items
+  after_action :verify_policy_scoped, only: :select_items, unless: :skip_pundit?
   # after_action :verify_authorized, except: :index, unless: :skip_pundit?
   # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   def index
+    @category = params[:category]
     if current_user
       @dailytarget = DailyTarget.find_by(user_id: current_user.id)
-      set_items
+      @items = select_items(category: @category)
     else
       @items = policy_scope(Item).seller
     end
 
+    @items_unfiltered = Item.all.seller
+
     @order_item = OrderItem.new
     @categories = Category.all
     @order = current_user&.pending_order
+
+    # deleteme
+    # binding.pry
+
   end
 
   # Para el tracking
@@ -80,13 +87,24 @@ class ItemsController < ApplicationController
     )
   end
 
-  def set_items
-    @items = []
-    policy_scope(Item).seller.each do |item|
+  def select_items(args = {})
+    item_selection = []
+
+    # Filter by category
+    if args[:category]
+      items = Item.all.seller.where(category: args[:category])
+    else
+      items = Item.all.seller
+    end
+
+    # Filter by nustritional target
+    items.seller.each do |item|
       if calories_in_target?(item) && proteins_in_target?(item) && carbs_in_target?(item) && fats_in_target?(item)
-        @items << item
+        item_selection << item
       end
     end
+
+    item_selection
   end
 
   def calories_in_target?(item)
