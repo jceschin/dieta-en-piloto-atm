8d5454253ad38269b3e41697e9622eb0dec94b50
JuanPablo CastiglioniDupin <jpcastiglioni@gmail.com>
2020-08-29 15:58:41 -0300

    accent in Seguí

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
     end
 
     @message = create_main_message(@category, @items)
-    @secondary_message = "Segui mirando nuestras opciones"
+    @secondary_message = "Seguí mirando nuestras opciones"
     @items_unfiltered = Item.all.seller
 
     @order_item = OrderItem.new
